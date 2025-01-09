9cb619b0216f9a3e237ab3bb14739e2c2f308532
OupaPie <138789562+OupaPie@users.noreply.github.com>
2025-01-09 08:35:12 +0200

    Add files via upload
    Initial Commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter } from "next/font/google";
+import { Quicksand } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const quicksand = Quicksand({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
